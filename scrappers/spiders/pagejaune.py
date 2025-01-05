99b74f0e8a82c725ad1b81e23eca29a9bc9928f1
saadaoui-salah <salahsaadaoui8@gmail.com>
2025-01-05 22:54:34 +0100

    add: cookies

--- scrappers/spiders/pagejaune.py
+++ scrappers/spiders/pagejaune.py
@@ -20,6 +20,9 @@ class PagejauneSpider(Spider):
     categories = ['Restaurant', 'Electricien', 'Beauté', 'Pharmacie', 'Kiné', 'Taxi', 'Comptable', 'Notaire', 'Plombier',]
     
     def start_requests(self):
+        self.cookies = {
+            "cf_clearance": "eZACHT.TQb9LAT1RnkHFa7iBUEqI1qWQrfKVtD7HFmo-1731094418-1.2.1.1-n8z.5yjBFJY7VjOygRlRp98IOks71_alCkK2MYCU5imaQ5nuhSsNm4MsWNwkuV3Qp_gp1Zd0UAFtz6uKjf9LLbrMnGnHcRCARcxSXXA3VFSBK4Sx.fYnBmV2m2PYz9l4Hj.fTSKpDEIUzak6mx6Vsc6xAThddHYV9qN24LUTPOFecnOi6i029vdXAuL.fDsH8bL390CniJRLMbn4cS0GSY7AcALkjzDdhyCZawBrmo5KySfK8fLPKQpu6enjC1DdRnoCNCe9p9JIcA60bRjSk6PMhOJI8bG17itb7I_rhDd9o8vdHRgZT8MpEswMG5S0qPh0gALM2v9qyjk5ETkRLyNUC953u6jlKQGLgYXcDySx31ml3dR4x1XF4HIo8EBJKDEM.mEPwshkeff3eOAcYDu6C19xxVnBYsOpjzu34QQ"
+        }
         self.headers = {
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
@@ -37,6 +40,7 @@ class PagejauneSpider(Spider):
                 yield Request(
                     url=f"https://www.pagesjaunes.fr/annuaire/chercherlespros?quoiqui={quote(category)}&ou={quote(location)}&univers=pagesjaunes&idOu=",
                     headers=self.headers,
+                    cookies=self.cookies,
                     callback=self.parse,
                     meta={'keyword': self.locations[0], 'category':category}
                 )
