26781c25dc2e0bb5c8f9f0eee97ab3abac693d1b
saadaoui-salah <salahsaadaoui8@gmail.com>
2025-02-16 23:49:20 +0100

    increase CONCURRENT_REQUESTS

--- scrappers/settings.py
+++ scrappers/settings.py
@@ -21,7 +21,7 @@ DOWNLOADER_MIDDLEWARES = {
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 16
+CONCURRENT_REQUESTS = 100
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
