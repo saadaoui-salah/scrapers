8ef68729503f32afcc78edc9442edf4e6042e639
saadaoui-salah <salahsaadaoui8@gmail.com>
2025-02-16 04:24:10 +0100

    client fixes

--- scrappers/spiders/blueridgeknives.py
+++ scrappers/spiders/blueridgeknives.py
@@ -4,6 +4,7 @@ import json
 
 class Product(scrapy.Item):
     # define the fields for your item here like:
+    category = scrapy.Field()
     brand = scrapy.Field()
     title = scrapy.Field()
     description = scrapy.Field()
@@ -40,24 +41,7 @@ class Product(scrapy.Item):
 
 class BlueridgeknivesSpider(scrapy.Spider):
     name = "blueridgeknives"
-    start_urls = ["https://store.blueridgeknives.com/brands"]
-    cookies = {
-        "form_key": "oLnGo21HYeexLwkQ",
-        "mage-cache-storage": "{}",
-        "mage-cache-storage-section-invalidation": "{}",
-        "recently_viewed_product": "{}",
-        "recently_viewed_product_previous": "{}",
-        "recently_compared_product": "{}",
-        "recently_compared_product_previous": "{}",
-        "product_data_storage": "{}",
-        "mage-messages": "",
-        "persistent_shopping_cart": "ycEW8FIpvboteKmS4fSxumUDvKCaODWXRhIcNZ0Arkxh8oZyUK",
-        "PHPSESSID": "d3a7b45c5f8c71ffd7ce3d9ff62e8349",
-        "private_content_version": "1ae5634f6bba824aba570793696035c0",
-        "X-Magento-Vary": "9b39552580316e1912d12b04cf050a794f7b10f23fb6ca31d2537804684eeb52",
-        "mage-cache-sessid": "true",
-        "section_data_ids": '{%22customer%22:1739553741%2C%22compare-products%22:1739553741%2C%22last-ordered-items%22:1739553741%2C%22cart%22:1739553741%2C%22directory-data%22:1739553741%2C%22captcha%22:1739553741%2C%22instant-purchase%22:1739553741%2C%22loggedAsCustomer%22:1739553741%2C%22persistent%22:1739553741%2C%22review%22:1739553741%2C%22payments%22:1739553741%2C%22wishlist%22:1739553741%2C%22recently_viewed_product%22:1739553741%2C%22recently_compared_product%22:1739553741%2C%22product_data_storage%22:1739553741%2C%22paypal-billing-agreement%22:1739553741}'
-    }
+    start_urls = ["https://store.blueridgeknives.com/shop-by-category"]
     headers = {
         "dnt": "1",
         "pragma": "no-cache",
@@ -73,34 +57,69 @@ class BlueridgeknivesSpider(scrapy.Spider):
         "upgrade-insecure-requests": "1",
         "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"
     }
-
+    cookies = {
+        "form_key": "nNQEQSjXEXx6JB0u",
+        "mage-cache-storage": "{}",
+        "mage-cache-storage-section-invalidation": "{}",
+        "recently_viewed_product": "{}",
+        "recently_viewed_product_previous": "{}",
+        "recently_compared_product": "{}",
+        "recently_compared_product_previous": "{}",
+        "product_data_storage": "{}",
+        "persistent_shopping_cart": "ycEW8FIpvboteKmS4fSxumUDvKCaODWXRhIcNZ0Arkxh8oZyUK",
+        "PHPSESSID": "6421ce12d669dae1e66ac11fc208001c",
+        "private_content_version": "290521747738ad278ab4be30e42b22b3",
+        "X-Magento-Vary": "9b39552580316e1912d12b04cf050a794f7b10f23fb6ca31d2537804684eeb52",
+        "mage-cache-sessid": "true",
+        "mage-messages": "",
+        "section_data_ids": '{"customer":1739675512,"compare-products":1739675512,"last-ordered-items":1739675512,"cart":1739675512,"directory-data":1739675512,"captcha":1739675512,"instant-purchase":1739675512,"loggedAsCustomer":1739675512,"persistent":1739675512,"review":1739675512,"payments":1739675512,"wishlist":1739675512,"recently_viewed_product":1739675512,"recently_compared_product":1739675512,"product_data_storage":1739675512,"paypal-billing-agreement":1739675512}'
+    }
 
     def start_requests(self):
         yield scrapy.Request(
                 url=self.start_urls[0],
-                cookies=self.cookies,
                 headers=self.headers,
-                callback=self.parse,
+                cookies=self.cookies,
+                callback=self.parse_categories,
             )
 
-    def parse(self, response):
-        brands = response.css('.ambrands-brand-item')
-        for brand in brands:
+
+
+    def parse_categories(self, response):
+        categories = response.css('.mgz-image-link a::attr(href)').getall()
+        for category in categories:
             yield scrapy.Request(
-                url=f"{brand.css('a::attr(href)').get()}?product_list_limit=96",
-                callback=self.parse_brands,
-                cookies=self.cookies,
+                url=f"https://store.blueridgeknives.com/{category}",
+                callback=self.parse_sub_categories,
                 headers=self.headers,
-                meta={'brand':brand.css('a::attr(href)').get()}
+                cookies=self.cookies,
             )
 
-    def parse_brands(self, response):
+    def parse_sub_categories(self, response):
+        categories = response.css('.mgz-element-categories-list li')
+        parent_path = response.css('.title::text').get()
+        if not parent_path:
+            parent_path = response.url.split('/')[-1].replace('.html', '').replace('-', ' ')
+            response.meta = {'path': [parent_path]}
+            yield from self.parse_products(response)
+        else:
+            for category in categories:
+                path = [parent_path, category.css('a > span::text').get()]
+                yield scrapy.Request(
+                    url=f"{category.css('a::attr(href)').get()}?product_list_limit=96",
+                    callback=self.parse_products,
+                    headers=self.headers,
+                    cookies=self.cookies,
+                    meta={'path':path}
+                )
+
+    def parse_products(self, response):
         products = response.css('.product-item-photo::attr(href)').getall()
         for product in products:
             yield scrapy.Request(
                 url=product,
-                cookies=self.cookies,
                 headers=self.headers,
+                cookies=self.cookies,
                 callback=self.parse_pdp,
                 meta=response.meta
             )
@@ -108,9 +127,9 @@ class BlueridgeknivesSpider(scrapy.Spider):
         if next_link := response.css('.pages-item-next .next::attr(href)').get():
             yield scrapy.Request(
                 url=next_link,
-                callback=self.parse_brands,
-                cookies=self.cookies,
+                callback=self.parse_products,
                 headers=self.headers,
+                cookies=self.cookies,
                 meta=response.meta
             )
 
@@ -119,7 +138,8 @@ class BlueridgeknivesSpider(scrapy.Spider):
 
     def parse_pdp(self, response):
         item = Product()
-        item['brand'] = response.meta['brand']
+        item['brand'] = response.css('[itemprop="manufacturer"] a::text').get()
+        item['category'] = ' > '.join(response.meta['path'])
         item['title'] = response.css('[data-ui-id="page-title-wrapper"]::text').get()
         item['description'] = remove_tags(response.css('.description').get('')) + remove_tags(response.css('#product-attribute-specs-table').get(''))
         item['url'] = response.url
