88491faeca63c5865c7c0dd5ab5c7c202beeb25f
saadaoui-salah <salahsaadaoui8@gmail.com>
2025-03-21 03:47:59 +0100

    remove: print

--- core/proxy/oxylabs.py
+++ core/proxy/oxylabs.py
@@ -4,7 +4,6 @@ class ProxyMiddleware(object):
     # overwrite process request
     def process_request(self, request, spider):
         if hasattr(spider,'proxy'):
-            print(os.getenv('OXYLABS_RS'))
             if spider.proxy == 'oxy_rs':
                 request.meta['proxy'] = os.getenv('OXYLABS_RS')
             if spider.proxy == 'oxy_dc':
