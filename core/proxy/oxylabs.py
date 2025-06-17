941170dae95f6f3b107da217207be55b0c523285
saadaoui-salah <salahsaadaoui8@gmail.com>
2025-06-17 09:33:56 +0100

    update

--- core/proxy/oxylabs.py
+++ core/proxy/oxylabs.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 class ProxyMiddleware(object):
     # overwrite process request
@@ -8,5 +9,7 @@ class ProxyMiddleware(object):
                 request.meta['proxy'] = os.getenv('OXYLABS_RS')
             if spider.proxy == 'oxy_dc':
                 request.meta['proxy'] = os.getenv('OXYLABS_DC')
+            if spider.proxy == 'oxy_isp':
+                request.meta['proxy'] = os.getenv('OXYLABS_ISP').replace('PORT', random.choice([8001,8002,8003,8004,8005,8006,8007,8008,8009,8010]))
             if spider.proxy == 'burp':
                 request.meta['proxy'] = 'http://127.0.0.1:8080'
