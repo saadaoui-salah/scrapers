b120fbb91239ac9375594176534a1812c8c29fed
saadaoui-salah <salahsaadaoui8@gmail.com>
2025-06-18 22:30:11 +0100

    update

--- core/spiders/ebay.py
+++ core/spiders/ebay.py
@@ -1,13 +1,14 @@
 import scrapy
 from w3lib.html import remove_tags
+from core.utils.utils import read_json
 
 class EbaySpider(scrapy.Spider):
     custom_settings = {
         'CONCURRENT_REQUESTS': 1,
-        'DOWNLOAD_DELAY': 2
+        'COOKIES_ENABLED':False
     }
     name = "ebay"
-    start_urls = [f"https://www.ebay.co.uk/str/automationplanetuk?_ipg=72&_pgn={i+1}&_tab=shop&_ajax=itemFilter&_tabName=shop" for i in range(237)]
+    start_urls = [f"https://www.ebay.co.uk/str/automationplanetuk?_ipg=200&_pgn={i}&_tab=shop&_ajax=itemFilter&_tabName=shop" for i in [49]]
     headers = {
         "accept": "*/*",
         "accept-encoding": "gzip, deflate, br, zstd",
@@ -16,7 +17,6 @@ class EbaySpider(scrapy.Spider):
         "dnt": "1",
         "pragma": "no-cache",
         "priority": "u=0, i",
-        "referer": "https://www.ebay.co.uk/str/automationplanetuk",
         "sec-ch-ua": "\"Google Chrome\";v=\"134\", \"Not=A?Brand\";v=\"8\", \"Chromium\";v=\"134\"",
         "sec-ch-ua-mobile": "?0",
         "sec-ch-ua-platform": "\"Linux\"",
@@ -27,14 +27,21 @@ class EbaySpider(scrapy.Spider):
         "upgrade-insecure-requests": "1",
         "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
     }
+    proxy = 'oxy_isp'
+    data = read_json('ebay-2.json')
+    done = []
 
     def start_requests(self):
-        for url in self.start_urls:
-            yield scrapy.Request(
-                url=url,
-                callback=self.parse,
-                headers=self.headers,
-        ) 
+        for i in range(20):
+            for url in self.data:
+                yield scrapy.Request(
+                    url=url['url'],
+                    dont_filter=True,
+                    callback=self.parse_pdp,
+                    headers=self.headers,
+                    meta={'item': url}
+            ) 
+
     def parse(self, response):
         data = response.json()
         items = filter(lambda x: x['_type'] == 'CardContainer',data['modules']['LISTINGS_MODULE']['containers'])
@@ -48,12 +55,11 @@ class EbaySpider(scrapy.Spider):
 
 
     def parse_pdp(self, response):
-        yield {
-            'name':remove_tags(response.css('.x-item-title__mainTitle').get('')),
-            'condition':remove_tags(response.css('.x-item-condition-text .ux-textspans::text').get('')),
-            'price':response.css('.x-price-primary .ux-textspans::text').get(''),
-            'availability':response.xpath("//div[@id='qtyAvailability']//span[contains(text(), 'available')]/text()").get() or 'Last one',
-            'sold':response.xpath("//div[@id='qtyAvailability']//span[contains(text(), 'sold')]/text()").get(),
-            'item specifics':remove_tags(response.css('.ux-layout-section--features').get('')),
-            'Product Identifiers':remove_tags(response.css('.ux-layout-section-evo.ux-layout-section--productIdentifiers').get('')),
-        }
+        item = response.meta['item']
+        if not remove_tags(response.css('.x-item-title__mainTitle').get('')) or response.url in self.done:
+            return
+        self.done += [response.url]
+        item['sold'] = response.xpath("//div[@id='qtyAvailability']//span[contains(text(), 'sold')]/text()").get()
+        item['item specifics'] = remove_tags(response.css('.ux-layout-section--features').get(''))
+        item['Product Identifiers'] = remove_tags(response.css('.ux-layout-section-evo.ux-layout-section--productIdentifiers').get(''))
+        yield item
