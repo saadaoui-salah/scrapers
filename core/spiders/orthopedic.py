239984388189e9032c26f26c34ebaa12f9e028cc
saadaoui-salah <salahsaadaoui8@gmail.com>
2025-06-21 19:51:48 +0100

    update

---
+++ core/spiders/orthopedic.py
@@ -0,0 +1,31 @@
+import scrapy
+
+
+class OrthopedicSpider(scrapy.Spider):
+    name = "orthopedic"
+    start_urls = ["https://orthopedic.io/physio-therapists/ga/"]
+
+    def parse(self, response):
+        for city in response.css('.col-md-3 .mynav a'):
+            print(city.css('::text').get())
+            yield scrapy.Request(
+                url=f"https:{city.css('::attr(href)').get()}",
+                callback=self.parse_doctors,
+                meta={'city': city.css('::text').get()}
+            )
+
+    def parse_doctors(self, response):
+        for doc in response.css('.doclist'):
+            yield {
+                'name': doc.css('.inline a::text').get(),
+                'phone': doc.xpath("./small/span[contains(@class, 'glyphicon-phone')]/following-sibling::text()[1]").get('').replace('\xa0', ''),
+                'fax': doc.xpath("./small/span[contains(@class, 'glyphicon-print')]/following-sibling::text()[1]").get(),
+                'city': response.meta['city']
+            }
+        
+        if next_page := response.css('[rel="next"]::attr(href)').get():
+            yield scrapy.Request(
+                url=f"https:{next_page}",
+                callback=self.parse_doctors,
+                meta=response.meta
+            )
