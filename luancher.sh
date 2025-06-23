20285a06fd425dbcd734638e363ffe1c4f230dfb
saadaoui-salah <salahsaadaoui8@gmail.com>
2025-06-23 19:01:28 +0100

    add: luncher

---
+++ luancher.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source ~/scrappers/env/bin/activate
+
+# Navigate to the Scrapy project directory
+cd ~/scrappers
+
+# Run the spider
+scrapy crawl suruga-ya
