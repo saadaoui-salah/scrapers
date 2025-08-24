355c3427057c14893f748f3dcf803a06dc45a7a7
saadaoui-salah <salahsaadaoui8@gmail.com>
2025-08-24 15:29:24 +0100

    update

--- core/spiders/reonomy.py
+++ core/spiders/reonomy.py
@@ -3,11 +3,11 @@ from core.utils.utils import read_json_files,  fake_request
 from datetime import datetime
 from core.proxy.zyte_api import ZyteRequest, load
 import json
+import pandas as pd 
 AUTH_TOKEN = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlJUWkdOamN3TlVVMFJFTTJORGt6UlRNd1JVSTVSVGs1TlVZeE56UkRNVUUzUlVNd09UTkVOdyJ9.eyJpc3MiOiJodHRwczovL2F1dGgucmVvbm9teS5jb20vIiwic3ViIjoiYXV0aDB8NzZkNWJlMjAtYzk4Ni00YWY0LTg1ZGEtYmIyMjM2MmQzYmY0IiwiYXVkIjpbImh0dHBzOi8vYXBwLnJlb25vbXkuY29tL3YyLyIsImh0dHBzOi8vcmVvbm9teS1wcmQuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTc1NTk3ODcwOCwiZXhwIjoxNzU2MDY1MTA4LCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwiYXpwIjoiVVRxaklaZjVqcUUwUm9SQ0pQRDIxNmFUOUNXWnJxMkMifQ.Vr8GazuW10goXa5DiORTJirLrmV4dLBWaZSpVB4mw2Yi3KsDQhuQC_glU3udtTxj9DTUoDhlANJEnVd8AAulupHNvYEZjarAoLCyimINTtgpR_rt_TFLSiCNoFBjAK39Y6enmcr3xy_YGy1nzuyjh2c0yaUO0Rk4CwiS1Z_kuYJPy7uqKfCVqfxJCCkNPpL9c-LPx_NxIEQkqjRhJ-PbRYLRHAK22X_f2UceBXkXIcCp9NTIrqB6eD9uZuIMzmeFIjDnMPznhFv01oL8YHxu3jet1iPEIb6IAq6lTCagsCgFcSMbO4MKOOFXPq0LH6VmNT0BHnYKtUo-Eaoh_vUq8A'
 
 class ReonomySpider(scrapy.Spider):
     name = "reonomy"
-    proxy = 'burp'    
     headers = {
         "accept": "*/*",
         "accept-language": "fr-FR,fr;q=0.9,ar-DZ;q=0.8,ar;q=0.7,en-US;q=0.6,en;q=0.5",
@@ -17,6 +17,9 @@ class ReonomySpider(scrapy.Spider):
     }
     
     def start_requests(self):
+        df = pd.read_csv("./client-data/reonomy.csv")
+        links = df["Link"].dropna().tolist()
+        self.ids = [link.split('/')[-2] for link in links]
         url = "https://api.reonomy.com/v2/search/pins?offset=0&limit=10000"
         body = {
             "settings": {
@@ -57,9 +60,13 @@ class ReonomySpider(scrapy.Spider):
         data = load(response)
         ids = [item["id"] for item in data.get("items", [])]
         self.logger.info(f"Got {len(ids)} IDs")
-
-        for i in range(0, len(ids), 50):
-            batch = ids[i:i+50]
+        cc = []
+        for id_ in ids:
+            if id_ not in self.ids:
+                cc += [id_]
+        print(len(cc))
+        for i in range(0, len(cc), 40):
+            batch = cc[i:i+40]
             yield ZyteRequest(
                 url="https://api.reonomy.com/v2/property/summaries",
                 method="POST",
@@ -72,50 +79,53 @@ class ReonomySpider(scrapy.Spider):
         dt = datetime.strptime(date, "%Y-%m-%d")
         return dt.strftime("%b %d, %Y")
 
-    def parse_details(self, response):
-        response = load(response)
-
+    def parse_details(self, r):
+        response = load(r)
         for item in response:
-            in_preforeclosure = item['in_preforeclosure']  if isinstance(item.get('in_preforeclosure'), bool) else  '-'
-            in_preforeclosure = 'Yes' if in_preforeclosure else 'No'
-            data = {
-                'Address': f"{item.get('house_nbr','')} {item.get('street')} {item.get('mode','').title()}, {item.get('city','').title()}, {item.get('state')} {item.get('zip5')}",
-                'APN': item['formatted_apn'],
-                'County':item['fips_county'],
-                'Lot Size (Acreage)':round(item['lot_size_acres'], 2) if item.get('lot_size_acres') else '--',
-                'Last Sale': self.format_date(item['sales_date'])  if item.get('sales_date') else  '--',
-                'Zoning': item['zoning']  if item.get('zoning') else  '--',
-                'Year Built': item['year_built']  if item.get('year_built') else  '--',
-                'Sale Price': f"${item['sales_price']}"  if item.get('sales_price') else  '--',
-                'Last Mortgage': self.format_date(item['mortgage_recording_date'])  if item.get('mortgage_recording_date') else  '--',
-                'Mortgage Amount': f"${item['mortgage_amount']}"  if item.get('mortgage_amount') else  '--',
-                'In Preforeclosure': in_preforeclosure,
-                'On CMBS Watchlist': item['on_watchlist']  if item.get('on_watchlist') else  '--',
-                'Building Area': f"{round(item['building_area'])} SF"  if item.get('building_area') else  '--',
-                'Recorded Lender': item['mortgage_recorded_name']  if item.get('mortgage_recorded_name') else  '--',
-                'Standardized Lender': item['mortgage_standardized_name']  if item.get('mortgage_standardized_name') else  '--',
-                'Seller': item['seller_name']  if item.get('seller_name') else  '--',
-                'Tax Amount': f"${round(item['tax_amount'])}"  if item.get('tax_amount') else  '--',
-                'Total Assessed Value': f"${round(item['total_assessed_value'])}"  if item.get('total_assessed_value') else  '--',
-                'Units': item['total_units']  if item.get('total_units') else  '--',
-                'Property Type': f"{item['asset_category']} | {item['std_land_use_code_description']}"  if item.get('asset_category') else  '--',
-                'Reported Owner': ', '.join([owner['name'] for owner in item['reported_owners']])  if item.get('reported_owners') else  '--',
-                'Sale Price/Acre': f"${round(item['price_per_acre_lot_area'])}"  if item.get('price_per_acre_lot_area') else  '--',
-                'Reported Owner Address': item['reported_owner_address']  if item.get('reported_owner_address') else  '--',
-                'Link': f"https://app.reonomy.com/!/search/467ce1b7-0ab8-4b3e-9c43-2c678011b56d/property/{item['id']}/ownership"
-            }
+            try:
+                in_preforeclosure = item['in_preforeclosure']  if isinstance(item.get('in_preforeclosure'), bool) else  '-'
+                in_preforeclosure = 'Yes' if in_preforeclosure else 'No'
+                data = {
+                    'Address': f"{item.get('house_nbr','')} {item.get('street')} {item.get('mode','').title()}, {item.get('city','').title()}, {item.get('state')} {item.get('zip5')}",
+                    'APN': item['formatted_apn'],
+                    'County':item['fips_county'],
+                    'Lot Size (Acreage)':round(item['lot_size_acres'], 2) if item.get('lot_size_acres') else '--',
+                    'Last Sale': self.format_date(item['sales_date'])  if item.get('sales_date') else  '--',
+                    'Zoning': item['zoning']  if item.get('zoning') else  '--',
+                    'Year Built': item['year_built']  if item.get('year_built') else  '--',
+                    'Sale Price': f"${item['sales_price']}"  if item.get('sales_price') else  '--',
+                    'Last Mortgage': self.format_date(item['mortgage_recording_date'])  if item.get('mortgage_recording_date') else  '--',
+                    'Mortgage Amount': f"${item['mortgage_amount']}"  if item.get('mortgage_amount') else  '--',
+                    'In Preforeclosure': in_preforeclosure,
+                    'On CMBS Watchlist': item['on_watchlist']  if item.get('on_watchlist') else  '--',
+                    'Building Area': f"{round(item['building_area'])} SF"  if item.get('building_area') else  '--',
+                    'Recorded Lender': item['mortgage_recorded_name']  if item.get('mortgage_recorded_name') else  '--',
+                    'Standardized Lender': item['mortgage_standardized_name']  if item.get('mortgage_standardized_name') else  '--',
+                    'Seller': item['seller_name']  if item.get('seller_name') else  '--',
+                    'Tax Amount': f"${round(item['tax_amount'])}"  if item.get('tax_amount') else  '--',
+                    'Total Assessed Value': f"${round(item['total_assessed_value'])}"  if item.get('total_assessed_value') else  '--',
+                    'Units': item['total_units']  if item.get('total_units') else  '--',
+                    'Property Type': f"{item['asset_category']} | {item['std_land_use_code_description']}"  if item.get('asset_category') else  '--',
+                    'Reported Owner': ', '.join([owner['name'] for owner in item['reported_owners']])  if item.get('reported_owners') else  '--',
+                    'Sale Price/Acre': f"${round(item['price_per_acre_lot_area'])}"  if item.get('price_per_acre_lot_area') else  '--',
+                    'Reported Owner Address': item['reported_owner_address']  if item.get('reported_owner_address') else  '--',
+                    'Link': f"https://app.reonomy.com/!/search/467ce1b7-0ab8-4b3e-9c43-2c678011b56d/property/{item['id']}/ownership"
+                }
 
 
-            url = f"https://api.reonomy.com/v3/property-contacts/{item['id']}"
-     
+                url = f"https://api.reonomy.com/v3/property-contacts/{item['id']}"
+        
 
-            yield ZyteRequest(
-                url=url,
-                method="GET",
-                headers=self.headers,
-                callback=self.parse_contacts_ids,
-                meta={'data':data}
-            )
+                yield ZyteRequest(
+                    url=url,
+                    method="GET",
+                    headers=self.headers,
+                    callback=self.parse_contacts_ids,
+                    meta={'data':data}
+                )
+            except:
+                print(r.request.body)
+                pass
 
 
     def fetch_owners(self, owners, extra, meta):
